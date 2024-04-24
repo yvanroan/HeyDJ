027766d08db12aa178d807c04a30df51d9ae3b34
yvanroan <yvanroan@yahoo.fr>
2024-04-24 02:15:42 -0400

    fix issues over wss in prod

--- main.py
+++ main.py
@@ -3,51 +3,21 @@
 # https://stackoverflow.com/questions/22711087/flask-importerror-no-module-named-app
 # https://stackoverflow.com/questions/23340812/python-sqlite-table-a-has-no-column-named-x
 
-from app.models import App_user, DJ, Song
-from app import db, app
-
+from app import app
 from app.routes import socketio
-from pathlib import Path
-
-
-print("main")
-
-
-def seed_database():
-
-# #for test purposes
-#     app.app_context().push()
-
-    dj=DJ(username='orelsan', phone='2111110000', email='orelsan@example.com', password='abcd')
-
-    u1 = App_user()
-    u2 = App_user()
-
-    s1 = Song(name='a', artist='rocker', genre='rock')
-    s2 = Song(name='b', artist='rocker', genre='rock')
-    s3 = Song(name='c', artist='rocker', genre='rock')
-    s4 = Song(name='d', artist='rocker', genre='rock')
-    s5 = Song(name='e', artist='rocker', genre='rock')
-    
-
-    db.session.add(dj)
-    db.session.add(u1)
-    db.session.add(u2)
-    db.session.add(s1)
-    db.session.add(s2)
-    db.session.add(s3)
-    db.session.add(s4)
-    db.session.add(s5)
-    
-
-    db.session.commit()
-    print("the party is ready to start")
+import ssl, os
     
 
 if __name__ == "__main__":
-    # seed_database()
-    # app.run(debug=True)
-    socketio.run(app, debug=True)
+
+    if os.getenv('use_ssl', 'false') == 'true':
+        # SSL context for local development
+        # context = ('localhost.pem', 'localhost-key.pem')  # You need to install mkcert to get these.
+        socketio.run(app, certfile='localhost.pem', keyfile='localhost-key.pem', debug=True)
+    else:
+        # No SSL in production, Heroku handles SSL termination
+        socketio.run(app, debug=True)
+
 
 
 # if any change is done to the database you have to run these:
