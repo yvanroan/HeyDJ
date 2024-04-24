fe34d9b9a80c22044dd8185959fb6726fad9daad
yvanroan <yvanroan@yahoo.fr>
2024-04-24 00:49:58 -0400

    setting up things for heroku db url

--- config.py
+++ config.py
@@ -11,5 +11,9 @@ class Config(object):
     # and if that isn't defined, I'm configuring a database named app.db located in the main directory of the application
     
     DATABASE_URL = os.environ.get('HEROKU_POSTGRESQL_GRAY_URL', f'sqlite:///{os.path.join(basedir, "app.db")}')
+    
+    if DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+            
     # print(DATABASE_URL)
     SQLALCHEMY_DATABASE_URI = DATABASE_URL
